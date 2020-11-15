7bb9ed0c88412ea8c75989008448aec06c3ca4da
Kevin Cui <krazycavin@gmail.com>
2020-11-15 15:11:44 +0100

    Change method to fetch video file
    In the old method, a valid cf token is mandatory during the process in order to get a proper video link. However, the way to get a cf token is flaky.
    
    In the new method, instead of targeting the video link, it targets m3u8 playlist. First, download all segments from m3u8 playlist, and then merge all segments to final video file.

--- README.md
+++ README.md
@@ -8,7 +8,6 @@
 - [Installation](#installation)
 - [How to use](#how-to-use)
   - [Example](#example)
-- [Limitation](#limitation)
 - [Disclaimer](#disclaimer)
 - [You may like...](#you-may-like)
   - [Don't like animepahe? Want an alternative?](#dont-like-animepahe-want-an-alternative)
@@ -20,20 +19,7 @@
 - [pup](https://github.com/EricChiang/pup)
 - [fzf](https://github.com/junegunn/fzf)
 - [Node.js](https://nodejs.org/en/download/)
-- [cf-cookie](https://github.com/KevCui/cf-cookie)
-
-## Installation
-
-- Update submodule and install npm packages, run command:
-
-```bash
-$ git clone https://github.com/KevCui/animepahe-dl.git
-$ cd animepahe-dl
-$ git submodule init
-$ git submodule update
-$ cd bin
-$ npm i puppeteer-core commander
-```
+- [ffmpeg](https://ffmpeg.org/download.html)
 
 ## How to use
 
@@ -48,7 +34,7 @@ Options:
   -e <num1,num3-num4...>  Optional, episode number to download
                           multiple episode numbers seperated by ","
                           episode range using "-"
-  -l                      Optional, list video link only without downloading
+  -l                      Optional, show m3u8 playlost link without downloading videos
   -h | --help             Display this help message
 ```
 
@@ -72,13 +58,13 @@ $ ./animepahe-dl.sh -a 'attack on titan'
 - By default, anime slug is stored in `./anime.list` file. Download "One Punch Man" season 2 episode 3:
 
 ```bash
-$ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660 -e 3
+$ ./animepahe-dl.sh -s 0bdee09d-1992-3f54-f309-509eca1533eb -e 3
 ```
 
 - List "One Punch Man" season 2 all episodes:
 
 ```bash
-$ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660
+$ ./animepahe-dl.sh -s 0bdee09d-1992-3f54-f309-509eca1533eb
 [1] E1 2019-04-09 18:45:38
 [2] E2 2019-04-16 17:54:48
 [3] E3 2019-04-23 17:51:20
@@ -96,7 +82,7 @@ $ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660
 - Support batch downloads: list "One Punch Man" season 2 episode 2, 5, 6, 7:
 
 ```bash
-$ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660 -e 2,2,5,6,7
+$ ./animepahe-dl.sh -s 0bdee09d-1992-3f54-f309-509eca1533eb -e 2,5,6,7
 [INFO] Downloading Episode 2...
 ...
 [INFO] Downloading Episode 5...
@@ -110,7 +96,7 @@ $ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660 -e 2,2,5,6,7
 OR using episode range:
 
 ```bash
-$ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660 -e 2,5-7
+$ ./animepahe-dl.sh -s 0bdee09d-1992-3f54-f309-509eca1533eb -e 2,5-7
 [INFO] Downloading Episode 2...
 ...
 [INFO] Downloading Episode 5...
@@ -121,23 +107,18 @@ $ ./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660 -e 2,5-7
 ...
 ```
 
-- Display only video link, used to pipe into `mpv` or other media player:
+- Show only m3u8 playlist link, without downloading video file:
 
 ```bash
-$ mpv "$(./animepahe-dl.sh -s 82a257c6-d361-69e9-9c43-10b45032a660 -e 5 -l)"
+$ ./animepahe-dl.sh -s 0bdee09d-1992-3f54-f309-509eca1533eb -e 5 -l
+...
 ```
 
-OR the interactive way:
+It's useful to toss m3u8 into media player and stream:
 
 ```bash
-$ mpv "$(./animepahe-dl.sh -l | grep 'https://')"
+$ mpv --http-header-fields="Referer: https://kwik.cx/" "$(./animepahe-dl.sh -s 0bdee09d-1992-3f54-f309-509eca1533eb -e 5 -l)"
 ```
-
-## Limitation
-
-Recently, animepahe implemented Cloudflare DDoS prevention mechanism with reCAPTCHA challenge. Current method is to fetch necessary cookie value from browser opened by puppeteer. Using this method, user must solve reCAPTCHA correctly at least once per day. The reCAPTCHA page will be prompted in browser.
-
-Sometimes, cookie can be turned invalid by animepahe server for certain reason. In this case, the reCAPTCHA page will be prompted in browser in order to generate a valid cookie.
 
 ## Disclaimer
 
@@ -156,3 +137,7 @@ Check out this script [tvdb-cli](https://github.com/KevCui/tvdb-cli)
 ---
 
 <a href="https://www.buymeacoffee.com/kevcui" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-orange.png" alt="Buy Me A Coffee" height="60px" width="217px"></a>
+
+```
+
+```
--- animepahe-dl.sh
+++ animepahe-dl.sh
@@ -12,7 +12,7 @@
 #/   -e <num1,num3-num4...>  Optional, episode number to download
 #/                           multiple episode numbers seperated by ","
 #/                           episode range using "-"
-#/   -l                      Optional, list video link only without downloading
+#/   -l                      Optional, show m3u8 playlist link without downloading videos
 #/   -h | --help             Display this help message
 
 set -e
@@ -28,17 +28,15 @@ set_var() {
     _PUP=$(command -v pup)
     _FZF=$(command -v fzf)
     _NODE=$(command -v node)
-    _CHROME=$(command -v chromium)
+    _FFMPEG=$(command -v ffmpeg)
 
     _HOST="https://animepahe.com"
     _ANIME_URL="$_HOST/anime"
     _API_URL="$_HOST/api"
+    _REFERER_URL="https://kwik.cx/"
 
     _SCRIPT_PATH=$(dirname "$(realpath "$0")")
     _ANIME_LIST_FILE="$_SCRIPT_PATH/anime.list"
-    _BYPASS_CF_SCRIPT="$_SCRIPT_PATH/bin/getCFcookie.js"
-    _USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$($_CHROME --version | awk '{print $2}') Safari/537.36"
-    _CF_FILE="$_SCRIPT_PATH/cf_clearance"
     _SOURCE_FILE=".source.json"
 }
 
@@ -104,38 +102,6 @@ search_anime_by_name() {
     fi
 }
 
-get_token_and_cookie() {
-    # $1: download link
-    local l cf j t c
-
-    l=$(sed -E 's/.cx\/e/.cx\/f/' <<< "$1")
-
-    if [[ "$(is_cf_expired)" == "yes" ]]; then
-        cf=$(get_cf_clearance "$l" | tee "$_CF_FILE")
-    else
-        cf=$(cat "$_CF_FILE")
-    fi
-
-    [[ -z "$cf" ]] && print_error "Cannot fetch cf_clearance from $l!"
-
-    h=$($_CURL -sS -c - "$l" \
-        --header "User-Agent: $_USER_AGENT"  \
-        --header "cookie: cf_clearance=$cf")
-
-    j=$(grep 'decodeURIComponent' <<< "$h" | grep 'escape' | sed -E 's/return decodeURIComponent/return console.log/')
-
-    t=$($_NODE -e "$j" 2>&1 \
-        | grep '_token' \
-        | sed -E 's/.*_token%22%20value%3D%22//' \
-        | awk -F '%22' '{print $1}')
-
-    c=$(grep '_session' <<< "$h" | awk '{print $NF}')
-
-    [[ -z "$t" || -z "$c" ]] && (rm -f "$_CF_FILE" && print_error "Cannot fetch valid token, please retry!")
-
-    echo "$t $c"
-}
-
 get_anime_id() {
     # $1: anime slug
     $_CURL -sS "$_ANIME_URL/$1" \
@@ -178,6 +144,20 @@ get_episode_link() {
         | tail -1
 }
 
+get_playlist() {
+    # $1: episode link
+    local s l
+    s=$($_CURL -sS -H "Referer: $_REFERER_URL" "$1" \
+        | grep '<script>' \
+        | sed -E 's/<script>//')
+
+    l=$($_NODE -e "$s" 2>&1 \
+        | grep 'source=' \
+        | sed -E "s/.m3u8';.*/.m3u8/" \
+        | sed -E "s/.*const source='//")
+    echo "$l"
+}
+
 download_episodes() {
     # $1: episode number string
     local origel el uniqel
@@ -213,57 +193,20 @@ download_episodes() {
     done
 }
 
-get_cf_clearance() {
-    # $1: url
-    print_info "Wait for solving reCAPTCHA to visit $1..."
-    $_BYPASS_CF_SCRIPT -u "$1" -a "$_USER_AGENT" -p "$_CHROME" -s \
-        | $_JQ -r '.[] | select(.name == "cf_clearance") | .value'
-}
-
-is_cf_expired() {
-    local o
-    o="yes"
-
-    if [[ -f "$_CF_FILE" && -s "$_CF_FILE" ]]; then
-        local d n
-        d=$(date -d "$(date -r "$_CF_FILE") +1 days" +%s)
-        n=$(date +%s)
-
-        if [[ "$n" -lt "$d" ]]; then
-            o="no"
-        fi
-    fi
-
-    echo "$o"
-}
-
 download_episode() {
     # $1: episode number
-    local l s t c ol rl
-
+    local l pl
     l=$(get_episode_link "$1")
     [[ "$l" != *"/"* ]] && print_error "Wrong download link or episode not found!"
 
-    s=$(get_token_and_cookie "$l")
-    t=$(echo "$s" | awk '{print $1}')
-    c=$(echo "$s" | awk '{print $NF}')
+    pl=$(get_playlist "$l")
+    [[ -z "${pl:-}" ]] && print_error "Missing video list!"
 
-    ol=$(sed -E 's/.cx\/e/.cx\/d/' <<< "$l")
-    rl=$(sed -E 's/.cx\/e/.cx\/f/' <<< "$l")
     if [[ -z ${_LIST_LINK_ONLY:-} ]]; then
         print_info "Downloading Episode $1..."
-        $_CURL -L "$ol" \
-            -H "Referer: $rl" \
-            -H "Cookie: kwik_session=$c" \
-            --data "_token=$t" -g -o "$_SCRIPT_PATH/${_ANIME_NAME}/${1}.mp4"
+        $_FFMPEG -headers "Referer: $_REFERER_URL" -i "$pl" -c copy "$_SCRIPT_PATH/${_ANIME_NAME}/${1}.mp4"
     else
-        $_CURL -sSD - "$ol" \
-            -H "Referer: $rl" \
-            -H "Cookie: kwik_session=$c" \
-            --data "_token=$t" \
-        | grep "location:" \
-        | tr -d '\r' \
-        | awk -F ': ' '{print $2}'
+        echo "$pl"
     fi
 }
 
