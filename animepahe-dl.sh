6e41cc60bb9b609c84fa2f1a563d2cc583c5f36e
Kevin Cui <krazycavin@gmail.com>
2021-01-22 10:11:56 +0100

    Change sh to bash when using xargs

--- animepahe-dl.sh
+++ animepahe-dl.sh
@@ -269,7 +269,7 @@ download_segments() {
     export _CURL _REFERER_URL op
     export -f download_file
     xargs -n 1 -I {} -P "$(get_thread_number "$1")" \
-        sh -c 'url="{}"; file="${url##*/}.encrypted"; download_file "$url" "${op}/${file}"' < <(grep "^https" "$1")
+        bash -c 'url="{}"; file="${url##*/}.encrypted"; download_file "$url" "${op}/${file}"' < <(grep "^https" "$1")
 }
 
 generate_filelist() {
@@ -293,7 +293,7 @@ decrypt_segments() {
     export _OPENSSL k
     export -f decrypt_file
     xargs -n 1 -I {} -P "$(get_thread_number "$1")" \
-        sh -c 'decrypt_file "{}" "$k"' < <(ls "${2}/"*.ts.encrypted \
+        bash -c 'decrypt_file "{}" "$k"' < <(ls "${2}/"*.ts.encrypted \
         | sed -E 's/ /\\ /g')
 }
 
