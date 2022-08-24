f09f43847927214c9e5220be50298ce056a7c38a
Kevin Cui <krazycavin@gmail.com>
2022-08-24 20:55:34 +0200

    Support variable declaring of non-default nodejs binary
    Issue #73

--- animepahe-dl.sh
+++ animepahe-dl.sh
@@ -32,7 +32,11 @@ set_var() {
     _CURL="$(command -v curl)" || command_not_found "curl"
     _JQ="$(command -v jq)" || command_not_found "jq"
     _FZF="$(command -v fzf)" || command_not_found "fzf"
-    _NODE="$(command -v node)" || command_not_found "node"
+    if [[ -z ${ANIMEPAHE_DL_NODE:-} ]]; then
+        _NODE="$(command -v node)" || command_not_found "node"
+    else
+        _NODE="$ANIMEPAHE_DL_NODE"
+    fi
     _FFMPEG="$(command -v ffmpeg)" || command_not_found "ffmpeg"
     if [[ ${_PARALLEL_JOBS:-} -gt 1 ]]; then
        _OPENSSL="$(command -v openssl)" || command_not_found "openssl"
